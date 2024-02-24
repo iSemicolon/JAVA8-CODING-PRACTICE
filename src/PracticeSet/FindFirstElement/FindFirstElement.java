031fb0dce63bd51d11b2d6c86769ac52ca7f0cc9
iSemicolon <palash.samanta7497@gmail.com>
2024-02-24 22:19:27 +0530

    " find the first element of the list "

---
+++ src/PracticeSet/FindFirstElement/FindFirstElement.java
@@ -0,0 +1,16 @@
+package PracticeSet.FindFirstElement;
+
+import java.util.Arrays;
+import java.util.List;
+
+//Given the list of integers, find the first element of the list using Stream functions?
+public class FindFirstElement {
+
+    public static void main(String[] args) {
+
+        List<Integer> list= Arrays.asList(10,15,8,49,25,98,98,32,15);
+
+        list.stream().findFirst().ifPresent(System.out::println);
+
+    }
+}
